3bf6ab31cf2d8d537e8344f72b6681c27168f4f8
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-05 10:14:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $dbconn = pg_connect(getenv("DATABASE_URL"));
 
 // Performing SQL query
-$query = "CREATE TABLE marios (\n	fart VARCHAR ( 50 ) NOT NULL \n);";
+$query = "ALTER TABLE fart \nADD COLUMN sexy VARCHAR;
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 // Printing results in HTML
