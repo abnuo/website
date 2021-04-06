103590ab844b25b8ef54c097e349dbcfaaa575a4
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-06 10:04:42 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,11 @@
 <body>
 <h1>The WebSite Of An Abnuos</h1>
 <a href="/google.php">My google proxy</a>
+<?php
+$poop = array("Free", "XXX", "Porn", "Rule34", "Tits", "Ass", "3gp", "video");
+shuffle($poop)
+$penis = implode(" ", $poop);
+echo "<p>$penis</p>";
+?>
 </body>
 </html>
