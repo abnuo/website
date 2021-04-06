713ceb1d3ed72839a0221b40fd4b783f6f2a77df
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-06 11:20:58 +0900

    Update parked.php

--- parked.php
+++ parked.php
@@ -1,5 +1,5 @@
 <h1><?php echo $_SERVER['HTTP_HOST']; ?></h1>
 <?php
 $datey = date("Y");
-echo "Copyright &copy; $datey $_SERVER['HTTP_HOST']. All Rights Reserved."
+echo "Copyright &copy; $datey $_SERVER['HTTP_HOST']. All Rights Reserved.";
 ?>
