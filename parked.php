a3fb96ea57e1027e0f29c87bc45a8788bc562272
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-06 11:56:44 +0900

    Update parked.php

--- parked.php
+++ parked.php
@@ -1,11 +1,13 @@
 <h1>Welcome to <?php echo $_SERVER['HTTP_HOST']; ?></h1>
 <?php
 $marios = array("Danky", "Yeety", "Dankest", "Derpiest", "Memiest", "Doggo", "Boi", "Yeetus", "Among Us", "Sponge Adventure Bob", "Play Now");
-shuffle($marios);
-$copy = $marios;
-$url = implode("-", $marios);
-$joinderp = implode(" ", $copy);
-echo "<a href=\"$url.com\">$joinderp</a>";
+for ($i = 1; $i <= 10; $i++) {
+    shuffle($marios);
+    $copy = $marios;
+    $url = implode("-", $marios);
+    $joinderp = implode(" ", $copy);
+    echo "<a href=\"http://$url.com\">$joinderp</a>\n<br>";
+}
 ?>
 <?php
 $datey = date("Y");
