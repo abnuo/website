567946ae2083d2fdb93547a61185b6a4f74de1b3
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-09 04:55:25 +0900

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,7 +1,7 @@
 <?php
-$ip = $_SERVER["HTTP_X_FORWADED_FOR"]
+$ip = $_SERVER["HTTP_X_FORWADED_FOR"];
 if (ip2long($ip) == false) {
-    $ip = "Probably a malicious script or an attempt at SQL injection."
+    $ip = "Probably a malicious script or an attempt at SQL injection.";
 }
 echo "<hr>\n<p>Your IP: " . $ip . "</p>"; 
 ?>
