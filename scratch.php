1d6da2974381bc762e0412fc9ca416aef7637a7c
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-09 03:31:02 +0900

    Update scratch.php

--- scratch.php
+++ scratch.php
@@ -1,1 +1,1 @@
-<iframe src="https://jamesbmadden.github.io/scratch-silicon/#<?php echo mt_rand(104, 510767418); ?>" width="480px" height="480px" allowFullscreen></iframe>
+<iframe src="https://jamesbmadden.github.io/scratch-silicon/#<?php echo mt_rand(104, 510767418); ?>" width="480px" height="480px"></iframe>
