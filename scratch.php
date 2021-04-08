781a579ce66d75e2b568493e746411deac065cb5
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-09 03:39:02 +0900

    Update scratch.php

--- scratch.php
+++ scratch.php
@@ -1,4 +1,4 @@
-<iframe src="https://jamesbmadden.github.io/scratch-silicon/#<?php echo mt_rand(104, 510767418); ?>" width="780px" height="780px"></iframe>
+<iframe src="https://jamesbmadden.github.io/scratch-silicon/#<?php echo mt_rand(104, 510767418); ?>" width="485" height="402"></iframe>
 <button onclick="giveAnother()">Give me another!</button>
 <script>
 function giveAnother() {
