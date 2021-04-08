0e3d199f37c6867ef8ad964bcbeb2e1f6d87d020
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-09 05:26:21 +0900

    Update ip.php

--- ip.php
+++ ip.php
@@ -3,5 +3,8 @@ $ip = end(array_values(array_filter(explode(',',$_SERVER['HTTP_X_FORWARDED_FOR']
 if (ip2long($ip) == false) {
     $ip = "Probably a malicious script or an attempt at SQL injection.";
 }
-echo "<hr>\n<p>Your IP: " . $ip . "</p>"; 
+echo "<hr>\n<p>Your IP: " . $ip . "</p>\n<br>"; 
 ?>
+<p>Charlie don't surf!</p>
+<br>
+<p>Inspired by <a href="danasoft.com">Danasoft</a></p>
