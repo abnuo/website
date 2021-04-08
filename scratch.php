636a3bf712fbcb0256f96e89abc2e52151d19716
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-09 03:54:35 +0900

    Update scratch.php

--- scratch.php
+++ scratch.php
@@ -1,7 +1,4 @@
-<iframe src="https://jamesbmadden.github.io/scratch-silicon/#<?php echo mt_rand(104, 510767418); ?>" width="485" height="402"></iframe>
-<button onclick="giveAnother()">Give me another!</button>
-<script>
-function giveAnother() {
-window.location.replace(window.location.href);
-}
-<script>
+<iframe src="https://jamesbmadden.github.io/scratch-silicon/#<?php $num = mt_rand(104, 510767418); echo $num; ?>" width="485" height="402"></iframe>
+<br>
+<p>Refresh for another project.<p>
+<p>Project link is <a href="https://scratch.mit.edu/projects/<?php echo $num?>/"><?php echo $num; ?></a></p>
