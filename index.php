e4119e031bd1cf128dfd96b6b00502e926a6dac7
abnuo <66645994+abnuo@users.noreply.github.com>
2021-04-13 00:37:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 <h1>The WebSite Of An Abnuos</h1>
-<a href="/google.php">My google proxy</a>
+<a href="/google.php">My google proxy</a> <a href="https://dazzling-voice-19912.pktriot.net">My other site (may be down; for stuff that requires a non-ephemeral filesystem/etc)</a>
 <?php
 $poop = array("Free", "XXX", "Porn", "Rule34", "Tits", "Ass", "3gp", "video");
 shuffle($poop);
