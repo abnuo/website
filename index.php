ef9af375cc9d5b62c9781a98b1b19a29fd2e0b65
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-02 05:53:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 <h1>The WebSite Of An Abnuos</h1>
-<a href="/google.php">My google proxy</a> <a href="https://dazzling-voice-19912.pktriot.net">My other site (may be down; for stuff that requires a non-ephemeral filesystem/etc)</a>
+<a href="/google.php">My google proxy</a> <a href="https://dazzling-voice-19912.pktriot.net">My other site (may be down; for stuff that requires a non-ephemeral filesystem/etc)</a> <a href="https://raproxy.herokuapp.com/">Womginx reverse proxy</a> <a href="https://altproxy.herokuapp.com/">Alternative proxies</a>
 <?php
 $poop = array("Free", "XXX", "Porn", "Rule34", "Tits", "Ass", "3gp", "video");
 shuffle($poop);
