39c2564b4f8ea6a0c4aa82e0e0c957da5e0e3e98
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-04 01:49:15 +0100

    Update arguments.js

--- arguments.js
+++ arguments.js
@@ -1,10 +1,25 @@
 "use strict";
 var system = require('system');
+function makeid(length) {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() * 
+ charactersLength)));
+   }
+   return result.join('');
+}
+
+var filename = makeid(5)
 if (system.args.length === 1) {
     console.log('Try to pass some args when invoking this script!');
 } else {
-    system.args.forEach(function (arg, i) {
-            console.log(i + ': ' + arg);
-    });
+    var page = require('webpage').create();
+    page.content = system.args[1];
+    page.render(filename + '.png');
+    phantom.exit();
+    system.args
 }
+console.log(filename);
 phantom.exit();
