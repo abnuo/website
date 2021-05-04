428961c6f406535bb387d5e3bdf9cb795bc49010
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-04 02:04:50 +0100

    Update tracery.php

--- tracery.php
+++ tracery.php
@@ -4,3 +4,13 @@ $sexypoop = trim(json_encode($poop), '*');
 echo $poop;
 require 'webhok.php';
 ?>
+<script>
+const Http = new XMLHttpRequest();
+const url='/phantom.php?html=' + encodeURIComponent(document.documentElement.innerHTML);
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
+</script>
