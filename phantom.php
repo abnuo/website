6904e24e149faf7b5dc310f37392562894696a98
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-04 02:10:53 +0100

    Update phantom.php

--- phantom.php
+++ phantom.php
@@ -1,3 +1,8 @@
 <?php
 $js = shell_exec("phantomjs arguments.js \"" . $_GET["html"] . "\"");
+$fp = fopen('names.txt', 'a');//opens file in append mode  
+fwrite($fp, $js + '\n');  
+fwrite($fp, 'appending data');  
+fclose($fp);  
+echo $js
 ?>
