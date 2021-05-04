31bdf45bc00f39bb74955d7294a4aa0130a67e85
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-04 01:23:08 +0100

    Update tracery.php

--- tracery.php
+++ tracery.php
@@ -1,6 +1,6 @@
 <?php
 $poop = shell_exec("node tracery.js");
 $sexypoop = trim(json_encode($poop), '*');
-shell_exec("curl -X POST -H \"Content-Type: application/json\" -d '{\"username\": \"AbnuoBot\", \"content\": \"$sexypoop\"}' \"https://discord.com/api/webhooks/831214997044199464/RC7kCRSqm-RDblyBUIqdOYy3icwU9IviZM3e5VAHXMY1FESez0lmfPHrFMlBye98x1-T\"");
 echo $poop;
+require 'webhok.php';
 ?>
