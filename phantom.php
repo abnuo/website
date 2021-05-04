fa24f336e5866dbc87f01f1047b586e8dc23a608
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-04 14:45:50 +0100

    Update phantom.php

--- phantom.php
+++ phantom.php
@@ -1,4 +1,4 @@
 <?php
-$js = shell_exec("phantomjs arguments.js \"" . str_replace('>', '\>', str_replace('<', '\<', addslashes($_GET["html"]))) . "\"");
+$js = shell_exec("phantomjs arguments.js \"" . str_replace('>', '\>', str_replace('<', '\<', $_GET["html"])) . "\"");
 echo $js
 ?>
