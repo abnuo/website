38dc78f4d893f188fd3c54b6b6bf0825c1a735d9
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-05 22:21:25 +0100

    Update comment.php

--- comment.php
+++ comment.php
@@ -3,7 +3,7 @@ $useragent = 'User with user agent ' . $_SERVER['HTTP_USER_AGENT'] . ' just comm
 $fp = fopen('comments.txt', 'a');
 fwrite($fp, $_GET["comment"] . "\n");
 fclose($fp);
-require 'webhok2.php';
 header('Location: /phpmyadmin');
+require 'webhok2.php';
 exit;
 ?>
