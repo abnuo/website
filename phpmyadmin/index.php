a5b23767ee2fd0d62973201e94408cf2729822dc
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-05 20:27:55 +0100

    Create index.php

---
+++ phpmyadmin/index.php
@@ -0,0 +1,14 @@
+<h1>phpMyAdmin</h1>
+
+<form>
+  <label for="comment">Please post your comments for the blog</label><br>
+  <br>
+  <textarea name="comment" id="comment" rows="4" columns="300"></textarea>
+  <br>
+  <input type="submit" name="submit" id="submit_comment" value="Submit">
+</form>
+<?php
+foreach(file("comments.txt") as $line) {
+    echo htmlspecialchars($line) . "\n<br>";
+}
+?>
