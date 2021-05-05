127fa7a777e9bae186bcb59f5efdbb23ccc8435e
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-05 22:21:03 +0100

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,5 +1,5 @@
 <?php
-$useragent = 'User with user agent ' . $_SERVER['HTTP_USER_AGENT'] . ' has commented.';
+$useragent = 'User with user agent ' . $_SERVER['HTTP_USER_AGENT'] . ' just commented ' . $_GET["comment"] . '.';
 $fp = fopen('comments.txt', 'a');
 fwrite($fp, $_GET["comment"] . "\n");
 fclose($fp);
