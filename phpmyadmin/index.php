5efad6c09c143ad763c6fcf58c66b05952e80777
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-05 23:09:01 +0100

    Update index.php

--- phpmyadmin/index.php
+++ phpmyadmin/index.php
@@ -5,7 +5,7 @@ function findInArray($array, $searchword) {
 }
 $dorksy = findInArray(file("dorks.txt"), "intitle:");
 $randIndex = array_rand($dorksy);
-$title = end(explode("intitle:", $dorksy[$randIndex]));
+$title = implode("", array_slice(explode('"', explode("intitle:", $dorksy[$randIndex])), 0, -1));
 echo "<title>$title</title>";
 ?>
 <h1><?php echo $title; ?></h1>
