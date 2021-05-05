011cb5654a9ce2fb01421b3723d518d03efc31a5
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-05 22:10:58 +0100

    Create webhok2.php

---
+++ webhok2.php
@@ -0,0 +1,18 @@
+<?php
+$url = 'https://discord.com/api/webhooks/831214997044199464/RC7kCRSqm-RDblyBUIqdOYy3icwU9IviZM3e5VAHXMY1FESez0lmfPHrFMlBye98x1-T';
+$data = array('username' => 'Sexypot', 'content' => $useragent);
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
+?>
