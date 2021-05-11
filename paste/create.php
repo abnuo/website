541da96bbe9ae47c91f070700ca87470e7834817
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-11 15:01:35 +0100

    Create create.php

---
+++ paste/create.php
@@ -0,0 +1,22 @@
+<?php
+$conn = pg_connect(getenv("DATABASE_URL"));
+$query = 'CREATE TABLE pastes ()';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+?>
