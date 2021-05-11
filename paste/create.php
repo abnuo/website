0ac3696bb16eda116475ca8280a085a17f71c44b
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-11 15:40:39 +0100

    Update create.php

--- paste/create.php
+++ paste/create.php
@@ -1,6 +1,17 @@
 <?php
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+$id = generateRandomString(5);
+$content = $_POST["content"];
 $conn = pg_connect(getenv("DATABASE_URL"));
-$query = "ALTER TABLE pastes\nADD COLUMN contents TEXT [];";
+$query = "INSERT INTO pastes\n    VALUES (ARRAY['$id'], ARRAY['$content']);";
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 // Printing results in HTML
