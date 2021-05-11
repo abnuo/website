d72f9e84102f74431de9cd86fa2bb86b8017b4b8
tatey12 <tatebaldwin788@gmail.com>
2021-05-11 11:22:29 -0400

    Update get.php

--- paste/get.php
+++ paste/get.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $id = $_GET['id'];
 $conn = pg_connect(getenv("DATABASE_URL"));
 $query = "SELECT * from pastes WHERE id='" . $id . "';";
