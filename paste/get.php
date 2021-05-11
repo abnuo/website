1ce3af4e2e292306be260e7e8e5817b6db66c9db
tatey12 <tatebaldwin788@gmail.com>
2021-05-11 11:20:15 -0400

    Update get.php

--- paste/get.php
+++ paste/get.php
@@ -1,10 +1,8 @@
 <?php
 $id = $_GET['id'];
 $conn = pg_connect(getenv("DATABASE_URL"));
-$query = "SELECT * from pastes;";
+$query = "SELECT * from pastes WHERE id=" . $id . ";";
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-
-// Printing results in HTML
 $arrayd = pg_fetch_array($result, null, PGSQL_ASSOC);
 $idpos = array_search($id,$arr,true);
 $contentpos = $idpos + 1;
@@ -14,9 +12,9 @@ echo "<h1>$id</h1>\n<textarea readonly rows="30" cols="60">$content</textarea>";
 
 
 
-// Free resultset
+// Free resultse
 pg_free_result($result);
 
-// Closing connection
+
 pg_close($dbconn);
 ?>
