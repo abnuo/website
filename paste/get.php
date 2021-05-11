7198b50740601c78292b23fb5c21414fb8e1209a
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-11 17:43:14 +0100

    Update get.php

--- paste/get.php
+++ paste/get.php
@@ -4,11 +4,24 @@ $conn = pg_connect(getenv("DATABASE_URL"));
 $query = "SELECT * from pastes;";
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
+function fatCat($n) {
+    return var_export($n, true);
+}
+function analVore($array) {
+    $matches = array();
+    foreach($example as $k=>$v) {
+        if(preg_match("/\b$searchword\b/i", $v)) {
+            $matches[$k] = $v;
+        }
+    }
+    return $matches;
+}
 // Printing results in HTML
 $arrayd = pg_fetch_array($result, null, PGSQL_BOTH);
+$blorpusYeeter = array_map('fatCat', $arrayd);
 $ids = $arrayd['penis'];
 $contents = $arrayd['contents'];
-$idpos = array_search($id,$arrayd,true);
+$idpos = analVore($blorpusYeeter)[0];
 $contentpos = $idpos + 1;
 if (strpos(trim($arrayd[$contentpos], '*'), '"') === 0) {
     $content = trim(trim($arrayd[$contentpos], '*'), '*');
