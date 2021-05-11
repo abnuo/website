41c8bb08b3ec06c5ccabafa47bd3d6feca022eee
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-11 18:10:24 +0100

    Update get.php

--- paste/get.php
+++ paste/get.php
@@ -1,9 +1,4 @@
 <?php
-$id = '{' . $_GET['id'] . '}';
-$conn = pg_connect(getenv("DATABASE_URL"));
-$query = "SELECT * from pastes;";
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
-
 function fatCat($n) {
     return var_export($n, true);
 }
@@ -27,12 +22,4 @@ if (strpos(trim($arrayd[$contentpos], '*'), '"') === 0) {
 }
 
 echo "<h1>$test:$ids:$contents:$idpos</h1>\n<textarea readonly rows=\"30\" cols=\"60\">$content</textarea>";
-
-
-
-// Free resultset
-pg_free_result($result);
-
-// Closing connection
-pg_close($conn);
 ?>
