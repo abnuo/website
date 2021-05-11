dc5dafa5070169ab52a74d4b4b407acedfb236cc
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-11 16:08:50 +0100

    Create get.php

---
+++ paste/get.php
@@ -0,0 +1,22 @@
+<?php
+$id = $_GET['id'];
+$conn = pg_connect(getenv("DATABASE_URL"));
+$query = "SELECT * from pastes;";
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+$arrayd = pg_fetch_array($result, null, PGSQL_ASSOC);
+$idpos = array_search($id,$arr,true);
+$contentpos = $idpos + 1;
+$content = htmlspecialchars($arrayd[$contentpos]);
+
+echo "<h1>$id</h1>\n<textarea readonly rows="30" cols="60">$content</textarea>";
+
+
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+?>
