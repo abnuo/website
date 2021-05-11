38813ab60dafc42f3345e596345b1e1fe9d2772f
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-11 19:22:33 +0100

    Update index.php

--- paste/index.php
+++ paste/index.php
@@ -1,5 +1,5 @@
 <h1>Paste</h1>
 <form action="create.php" method="POST">
-<textarea id="content" name="content" rows="30" cols="60"><br><br>
+<textarea id="content" name="content" rows="30" cols="60"></textarea><br><br>
 <input type="submit" value="Submit">
 </form>
