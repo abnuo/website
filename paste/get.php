155162d2fe24a7c0af7538b211ee93f5fa2a2db6
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-11 20:40:35 +0100

    Update get.php

--- paste/get.php
+++ paste/get.php
@@ -26,4 +26,4 @@ if (stripslashes(substr($arrayd[$contentpos], 1, -1))[0] == '"') {
 
 echo "<h1>$idpos:$id</h1>\n<textarea readonly rows=\"13\" cols=\"60\">" . htmlspecialchars($content) . "</textarea><br>";
 ?>
-<a href="/paste/raw.php?id=<?php echo $id; ?>">Raw</a><a href="/paste/">Create paste</a>
+<a href="/paste/raw.php?id=<?php echo $_GET['id']; ?>">Raw</a><a href="/paste/">Create paste</a>
