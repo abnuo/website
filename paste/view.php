b7f8c36c9470bef26d4a96bd774938123df6d179
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-11 18:34:52 +0100

    Update view.php

--- paste/view.php
+++ paste/view.php
@@ -1,31 +1,20 @@
 <?php
-header('Content-Type: text/plain');
+function getTheScat() {
+    $conn = pg_connect(getenv("DATABASE_URL"));
+    $query = "SELECT * from pastes;";
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-function generateRandomString($length) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    // Printing results
+    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+        foreach ($line as $col_value) {
+            echo "$col_value\n";
+        }
     }
-    return $randomString;
+
+    // Free resultset
+    pg_free_result($result);
+
+    // Closing connection
+    pg_close($conn);
 }
-$id = generateRandomString(5);
-$content = $_POST["content"];
-$conn = pg_connect(getenv("DATABASE_URL"));
-$query = "SELECT * from pastes;";
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
-
-// Printing results
-while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    foreach ($line as $col_value) {
-        echo "$col_value\n";
-    }
-}
-
-// Free resultset
-pg_free_result($result);
-
-// Closing connection
-pg_close($conn);
 ?>
