de9069771594ec929ac1bd594c0529fe14281659
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-11 15:41:33 +0100

    Create view.php

---
+++ paste/view.php
@@ -0,0 +1,33 @@
+<?php
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+$id = generateRandomString(5);
+$content = $_POST["content"];
+$conn = pg_connect(getenv("DATABASE_URL"));
+$query = "SELECT * from pastes;";
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+?>
