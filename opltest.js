e3dd6397336822ea31a4ae3780213ad673ad62cb
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-12 18:03:48 +0100

    Update opltest.js

--- opltest.js
+++ opltest.js
@@ -2,11 +2,12 @@ const OPL = require('@malvineous/opl');
 const size =  5000
 const amplitude = 128
 const sampleRate = 20
+let samples = []
 let wav = require('node-wav');
 
 OPL.create().then(opl => {
     opl.write(0xBD, 0x20);
-    const samples = opl.generate(512);
+    samples = opl.generate(512);
     // samples now contains 512 16-bit stereo samples as a Uint8Array
 });
 let buffer = wav.encode(samples, { sampleRate: sampleRate, float: true, bitDepth: 32 });
