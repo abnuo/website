700170d4064ed8cc950c8e071462cfda2fb105db
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-12 15:10:04 +0100

    Create char.php

---
+++ char.php
@@ -0,0 +1,12 @@
+<?php
+function uniord($u) {
+    $k = mb_convert_encoding($u, 'UCS-2LE', 'UTF-8');
+    $k1 = ord(substr($k, 0, 1));
+    $k2 = ord(substr($k, 1, 1));
+    return $k2 * 256 + $k1;
+}
+$chars = str_split($_GET['s']);
+foreach ($chars as $char) {
+    echo uniord($char)(
+}
+?>
