754e0dc2da65f5314bb4f2e51629d53490ec5dbc
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-12 18:18:33 +0100

    Update opltest.js

--- opltest.js
+++ opltest.js
@@ -1,19 +1,16 @@
 const OPL = require('@malvineous/opl');
+const fs = require('fs');
+const wavefile = require('wavefile');
 const size =  5000
 const amplitude = 128
 const sampleRate = 20
-let samples = []
-let wav = require('node-wav');
 
 OPL.create().then(opl => {
     opl.write(0xBD, 0x20);
-    samples = opl.generate(512);
+    const samples = opl.generate(512);
     // samples now contains 512 16-bit stereo samples as a Uint8Array
-});
-let buffer = wav.encode(samples, { sampleRate: sampleRate, float: true, bitDepth: 32 });
-
-fs.writeFile("test.wav", buffer, (err) => {
-  if (err) return console.log(err);
-
-  console.log("test.wav written");
+    let wav = new wavefile.WaveFile();
+    // Create a mono wave file, 44.1 kHz, 32-bit and 4 samples
+    wav.fromScratch(1, 44100, '16', samples);
+    fs.writeFileSync(path, wav.toBuffer());
 });
