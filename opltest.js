9ff2117fb3946e193e66c242d565a30e9af0f0a1
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-12 18:38:01 +0100

    Update opltest.js

--- opltest.js
+++ opltest.js
@@ -1,12 +1,23 @@
 const OPL = require('@malvineous/opl');
 const fs = require('fs');
 const wavefile = require('wavefile');
+const crypto = require('crypto'); 
 const size =  5000
 const amplitude = 128
 const sampleRate = 20
+crypto.randomBytes(127, (err, buf) => { 
 
+  if (err) { 
+
+    // Prints error 
+
+    console.log(err); 
+
+    return; 
+
+  } 
 OPL.create().then(opl => {
-    opl.write(0xBD, 0x20);
+    opl.write(buf);
     const samples = opl.generate(512);
     // samples now contains 512 16-bit stereo samples as a Uint8Array
     let wav = new wavefile.WaveFile();
