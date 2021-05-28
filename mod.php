dff32ab526f9b08582546d86048819769c8c96a5
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-28 01:14:19 +0100

    Create mod.php

---
+++ mod.php
@@ -0,0 +1,14 @@
+<?php
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+$fn = generateRandomString(5);
+shell_exec("ffmpeg -i " . $_GET["i"] . " " . $fn . ".mp3");
+echo "<a href=\"$fn\">Your file</a>";
+?>
