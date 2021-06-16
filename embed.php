ddf60a2f594364287b77086800077cbb5868d4a2
abnuo <66645994+abnuo@users.noreply.github.com>
2021-06-16 07:40:21 +0100

    Update embed.php

--- embed.php
+++ embed.php
@@ -4,7 +4,7 @@ if (isset($_GET["t"])) {
     echo "<meta name='og:title' content='{$_GET["t"]}' />";
 }
 if (isset($_GET["d"])) {
-    echo "<meta name='og:title' content='{$_GET["d"]}' />";
+    echo "<meta name='og:description' content='{$_GET["d"]}' />";
 }
 if (isset($_GET["i"])) {
     echo "<meta name='twitter:card' content='summary_large_image' />";
