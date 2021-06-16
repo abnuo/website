a12ff32886f5f8e64c9968ab84dc24d2616554be
abnuo <66645994+abnuo@users.noreply.github.com>
2021-06-16 01:03:51 +0100

    Create embed.php

---
+++ embed.php
@@ -0,0 +1,10 @@
+<h1>Discord Embed Generator</h1>
+<?php
+if (isset($_GET["t"])) {
+    echo "<meta name='og:title' content='{$_GET["t"]}' />";
+}
+if (isset($_GET["d"])) {
+    echo "<meta name='og:title' content='{$_GET["d"]}' />";
+}
+// Images WIP
+?>
