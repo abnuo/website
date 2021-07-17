4b3ab6ddcfca0adfb556fd5d70ab542e36378e93
abnuo <66645994+abnuo@users.noreply.github.com>
2021-07-18 00:41:50 +0100

    Update tenor.php

--- markovapi/tenor.php
+++ markovapi/tenor.php
@@ -1,7 +1,7 @@
 <?php
-q = $_GET["q"];
-l = $_GET["l"];
-tenors = explode("|", shell_exec("python tenor2.py \"$q\" $l"));
+$q = $_GET["q"];
+$l = $_GET["l"];
+$tenors = explode("|", shell_exec("python tenor2.py \"$q\" $l"));
 foreach ($tenors as $i) {
     echo "<img src=\"$i\" />";
 }
