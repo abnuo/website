4a84ab53dc378ed68c2488c6ecfe7d3d36c9b678
abnuo <66645994+abnuo@users.noreply.github.com>
2021-07-24 01:54:23 +0100

    Update stream.php

--- stream.php
+++ stream.php
@@ -1,5 +1,5 @@
 <h1>Streaming</h1>
 <?php
-shell_exec("ffmpeg -re -i \"" . $_GET["i"] "\" -i smurf.mp3 -vf \"loop=loop=-1:size=1:start=0\" -f flv rtmp://a.rtmp.youtube.com/live2/" . $_GET["key"]);
+shell_exec('ffmpeg -re -i "' . $_GET["i"] '" -i smurf.mp3 -vf \"loop=loop=-1:size=1:start=0\" -f flv rtmp://a.rtmp.youtube.com/live2/' . $_GET["key"]);
 echo "Starting stream...";
 ?>
