9b2e1b9902c9f178908c85a9134eaa241174bf29
abnuo <66645994+abnuo@users.noreply.github.com>
2021-08-01 17:50:06 +0100

    Update and rename ytembed.php to ytdl.php

---
+++ ytdl.php
@@ -0,0 +1,15 @@
+<?php
+function genStr($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+$filename = genStr(5);
+shell_exec("ytdl --output dl/$filename https://youtu.be/" . $_GET["id"] . ");
+echo "<a href=\"dl/$filename\">Your file</a>";
+?>
