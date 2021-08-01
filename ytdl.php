2f87ce840b712f54d9dfe516891604d69a2f8f33
abnuo <66645994+abnuo@users.noreply.github.com>
2021-08-01 17:58:17 +0100

    Update ytdl.php

--- ytdl.php
+++ ytdl.php
@@ -11,5 +11,5 @@ function genStr($length = 10) {
 
 $filename = genStr(5);
 shell_exec("yt-dlp --output dl/$filename https://youtu.be/" . $_GET["id"] . ");
-echo "<a href=\"dl/$filename\">Your file</a>";
+echo '<a href="dl/' . $filename . '">Your file</a>';
 ?>
