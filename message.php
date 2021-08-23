2f04284ee8ddd637624c0492fcd5e18d7c11720b
abnuo <66645994+abnuo@users.noreply.github.com>
2021-08-23 15:55:54 +0100

    Create message.php

---
+++ message.php
@@ -0,0 +1,41 @@
+<?php
+function replace_in_file($FilePath, $OldText, $NewText)
+{
+    $Result = array('status' => 'error', 'message' => '');
+    if(file_exists($FilePath)===TRUE)
+    {
+        if(is_writeable($FilePath))
+        {
+            try
+            {
+                $FileContent = file_get_contents($FilePath);
+                $FileContent = str_replace($OldText, $NewText, $FileContent);
+                if(file_put_contents($FilePath, $FileContent) > 0)
+                {
+                    $Result["status"] = 'success';
+                }
+                else
+                {
+                   $Result["message"] = 'Error while writing file';
+                }
+            }
+            catch(Exception $e)
+            {
+                $Result["message"] = 'Error : '.$e;
+            }
+        }
+        else
+        {
+            $Result["message"] = 'File '.$FilePath.' is not writable !';
+        }
+    }
+    else
+    {
+        $Result["message"] = 'File '.$FilePath.' does not exist !';
+    }
+    return $Result;
+}
+
+$new = replace_in_file('headers.txt', '{MESSAGE}', $_POST['message']);
+echo $new;
+?>
