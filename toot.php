0412ff060cf431ec9b0a7932058a6c390f2ed4fe
abnuo <66645994+abnuo@users.noreply.github.com>
2021-08-23 16:20:33 +0100

    Update toot.php

--- toot.php
+++ toot.php
@@ -1,6 +1,10 @@
 <h1>KrazyRobo Fart Jar</h1>
 <marquee><h1>Wanna Fart?</h1></marquee>
 <?php
-$toot = shell_exec("python bot.py");
+if (isset($_GET['n'])) {
+    $toot = shell_exec("python bot.py " . $_GET['n']);
+} else {
+    $toot = shell_exec("python bot.py");
+}
 echo $toot;
 ?>
