60554f9c193144bd4c370387e73035fe3d8952d4
abnuo <66645994+abnuo@users.noreply.github.com>
2021-08-31 23:57:07 +0100

    Create streamy.py

---
+++ streamy.py
@@ -0,0 +1,4 @@
+import os
+
+print('Starting stream.')
+os.system("echo 'main(t){for(;;t++)putchar(" + os.environ['expr'] + ");}' | cc -x c - -o bytebeat && ./bytebeat" + " | ffmpeg -re -f u8 -ar 8000 -ac 1 -i - -f flv rtmp://a.rtmp.youtube.com/live2/" + os.environ['key'])
