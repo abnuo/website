a868eee9901d009b5936df20f45c2fd9a80695de
abnuo <66645994+abnuo@users.noreply.github.com>
2021-09-01 00:20:19 +0100

    Update streamy.py

--- streamy.py
+++ streamy.py
@@ -1,4 +1,4 @@
 import os
 
 print('Starting stream.')
-os.system("echo 'main(t){for(;;t++)putchar(" + os.environ['expr'] + ");}' | cc -x c - -o bytebeat && ./bytebeat" + " | ffmpeg -re -f u8 -ar 8000 -ac 1 -i - -f flv rtmp://a.rtmp.youtube.com/live2/" + os.environ['key'])
+os.system("echo 'main(t){for(;;t++)putchar(" + os.environ['expr'] + ");}' | cc -x c - -o bytebeat && ./bytebeat" + " | ffmpeg -re -f u8 -ar 8000 -ac 1 -i - -f flv -ar 44100 rtmp://a.rtmp.youtube.com/live2/" + os.environ['key'])
