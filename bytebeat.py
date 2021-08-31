396f23c3c08a2b60646077514fc8f0f8eac21b59
abnuo <66645994+abnuo@users.noreply.github.com>
2021-08-31 20:12:31 +0100

    Create bytebeat.py

---
+++ bytebeat.py
@@ -0,0 +1,15 @@
+import sys
+import random
+import requests
+from itertools import count
+
+def gen(expr, dur):
+    try:
+        #for t in count(0)
+        #   sys.stdout.write(expr)
+        for t in dur
+            sys.stdout.write(expr)
+    except Exception as e:
+        print('Error Virus: ' + str(e))
+
+gen(sys.argv[1], sys.argv[2])
