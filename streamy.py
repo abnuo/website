48b590ecc62455aed3b44d4862bada933967aa56
abnuo <66645994+abnuo@users.noreply.github.com>
2021-09-01 14:06:35 +0100

    Update streamy.py

--- streamy.py
+++ streamy.py
@@ -1,4 +1,7 @@
 import os
 
-print('Starting stream.')
-os.system("echo 'main(t){for(;;t++)putchar(" + os.environ['expr'] + ");}' | cc -x c - -o bytebeat && ./bytebeat" + " | ffmpeg -re -f rawvideo -video_size 100x100 -pixel_format rgb8 -framerate 8 -i - -f u8 -ar 8000 -ac 1 -i - -f flv -ar 44100 rtmp://a.rtmp.youtube.com/live2/" + os.environ['key'])
+if os.environ['stream'] == 'Y':
+    print('Streaming is enabled, Starting stream.')
+    os.system("echo 'main(t){for(;;t++)putchar(" + os.environ['expr'] + ");}' | cc -x c - -o bytebeat && ./bytebeat" + " | ffmpeg -re -f rawvideo -video_size 100x100 -pixel_format rgb8 -framerate 25 -i - -f u8 -ar 8000 -ac 1 -i - -f flv -ar 44100 rtmp://a.rtmp.youtube.com/live2/" + os.environ['key'])
+else:
+    print("Streaming is disabled.")
