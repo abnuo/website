3c4420ff79452b507588a74a96d1bc96af01bae0
abnuo <66645994+abnuo@users.noreply.github.com>
2021-10-01 16:43:50 +0100

    Update wav.php

--- wav.php
+++ wav.php
@@ -1,8 +1,8 @@
 <?php
-header("Content-Type: audio/wav");
+header('Content-Type: audio/basic');
 $temp = tmpfile();
 $samples = 192000
-$bytes = [0, 0, 0, 32, 1, 72, 162, 24, 0, 0, 0, 3, 0, 0, 31, 64, 0, 0, 0, 1];
+$bytes = [0, 0, 0, 32, 1, 72, 162, 24, 0, 0, 0, 2, 0, 0, 31, 64, 0, 0, 0, 1];
 $bytesStr = pack('C*', ...$bytes);
 fwrite($temp, ".snd");
 // fwrite($temp, $bytesStr);
