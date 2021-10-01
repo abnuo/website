5b8e65714accdddacb02cf05d94cdf5461b0693d
abnuo <66645994+abnuo@users.noreply.github.com>
2021-10-01 17:03:53 +0100

    Update wav.php

--- wav.php
+++ wav.php
@@ -4,12 +4,15 @@ $temp = tmpfile();
 $samples = 192000;
 $bytes = [0, 0, 0, 32, 1, 72, 162, 24, 0, 0, 0, 2, 0, 0, 31, 64, 0, 0, 0, 1];
 $bytesStr = pack('C*', ...$bytes);
-fwrite($temp, ".snd");
+//fwrite($temp, ".snd");
+echo ".snd";
 // fwrite($temp, $bytesStr);
-file_put_contents($temp, $bytesStr);
+//file_put_contents($temp, $bytesStr);
+echo $bytesStr;
 for ($t = 0; $t <= $samples; $t++) {
-  fputs($temp, chr($t));
+  //fputs($temp, chr($t));
+  echo chr($t);
 }
-fseek($temp, 0);
-echo file_get_contents($temp);
+//fseek($temp, 0);
+//echo file_get_contents($temp);
 ?>
