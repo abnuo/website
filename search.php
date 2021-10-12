c42b525bcfe1d3115078b7c06d99c34eeef05559
abnuo <66645994+abnuo@users.noreply.github.com>
2021-10-12 16:31:46 +0100

    Update search.php

--- search.php
+++ search.php
@@ -20,9 +20,9 @@
       } else {
           $data = file_get_contents('https://www.google.com/search?q=' . $search);
       }
-      $data = str_replace("/search", "/search.php", $data);
-      $data = str_replace("/?sa", "/google.php?sa", $data);
-      $data = str_replace("/url", "/url.php", $data);
+      $data = str_replace("href=\"/search\"", "href=\"/search.php\"", $data);
+      $data = str_replace("href=\"/?sa\"", "href=\"/google.php?sa\"", $data);
+      $data = str_replace("href=\"/url\"", "href=\"/url.php\"", $data);
     } elseif ($mode == 'img') {
       if (isset($_GET['start'])) {
           $data = file_get_contents('https://www.google.com/search?q=' . $search . '&tbm=isch' . "&start=" . $_GET['start']);
