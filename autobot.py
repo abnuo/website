c8ae19a5cc4df5af6e16549d95121caf2e8438fb
abnuo <66645994+abnuo@users.noreply.github.com>
2021-10-31 12:43:47 +0000

    Update autobot.py

--- autobot.py
+++ autobot.py
@@ -2,10 +2,11 @@ import json
 import os
 import time
 
+command = "python bot.py & python newmaker.py"
 f = open('bot_config.json', 'r')
 config = json.loads(f.read())
 dur = config['dur']
 
 while True:
-    os.system('python bot.py')
+    os.system(command)
     time.sleep(dur)
