2aed87b424cb4361d4757737adf81114eb6f34a2
abnuo <66645994+abnuo@users.noreply.github.com>
2022-03-16 22:13:58 +0000

    Update pklmake.py

--- bot/pklmake.py
+++ bot/pklmake.py
@@ -3,6 +3,14 @@ import pickle
 import os
 from selenium.webdriver.chrome.options import Options
 options = Options()
+options.addArguments("--window-size=1920,1080"); 
+options.addArguments("--disable-gpu"); 
+options.addArguments("-- disable-extensions"); 
+options.setExperimentalOption("useAutomationExtension", false); 
+options.addArguments("--proxy-server='direct://'"); 
+options.addArguments("--proxy-bypass-list=*"); 
+options.addArguments("--start-maximized"); 
+options.addArguments("--headless"); 
 options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36")
 
 def getsession():
