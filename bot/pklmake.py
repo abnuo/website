fc705dcc53ff5b8569e9cd222452b1e9faec8f04
abnuo <66645994+abnuo@users.noreply.github.com>
2022-03-16 20:36:14 +0000

    Create pklmake.py

---
+++ bot/pklmake.py
@@ -0,0 +1,11 @@
+import twitter_selenium
+import pickle
+
+def getsession():
+  if not os.path.isfile("session.pkl"):
+    session = twitter_selenium.TwitterSession(os.environ["twitteruser"],os.environ["twitterpass"],driver="chrome")
+    f = open("session.pkl","wb")
+    pickle.dump(session.cookies,f)
+  else:
+    f = open("session.pkl","rb")
+    session = twitter.selenium(cookies=pickle.load(f),driver="chrome")
