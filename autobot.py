6d792263708c342eb55d762f1cd04460829beb05
abnuo <66645994+abnuo@users.noreply.github.com>
2022-03-16 20:47:06 +0000

    Update autobot.py

--- autobot.py
+++ autobot.py
@@ -4,7 +4,7 @@ import time
 import glob
 import random
 
-scripts = glob.glob("bot/*.py")
+scripts = [i for i in glob.glob("bot/*.py") if not i.endswith("pklmake.py")]
 script = random.choice(scripts)
 command = f"python {script}"
 f = open('bot_config.json', 'r')
